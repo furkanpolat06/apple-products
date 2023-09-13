bb0b207d8fc6696ce0b0fd2fe64236e664578fe3
furkanpolat06 <emnplt.0698@gmail.com>
2023-09-13 22:04:26 +0300

    card completed

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Card = ({name, image, price}) => {
+const Card = ({name, image, price, description}) => {
   return (
-    <div>
+    <div className='cards'>
+        <div className='name'>{name}</div>
         <img src={image} alt="jpg" />
+        <div className='price'>${price}</div>
+
+   
+        <div className='card-over'>{description} </div>
+    
+
     </div>
   )
 }
