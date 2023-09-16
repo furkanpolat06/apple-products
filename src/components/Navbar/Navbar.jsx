2d22e7a99d53ef071dd6ff7360d264d7d978597e
furkanpolat06 <emnplt.0698@gmail.com>
2023-09-16 07:47:16 +0300

    style:css edited

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,82 @@
 import logo from "../../helpers/logo.png";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import "./Navbar.scss";
 import { useState } from "react";
 
-const MyNavbar = ({ data, setSelectedCategory}) => {
-  
+const MyNavbar = ({ setSelectedCategory }) => {
+  const [activeCategory, setActiveCategory] = useState("");
+
   const handleCategoryFilter = (category) => {
     setSelectedCategory(category);
-    
- }
-const otherCategories = ["headphones", "accessories", "wearables"];
+    setActiveCategory(category);
+  };
 
   return (
-    <Navbar bg="dark" data-bs-theme="dark" expand="lg " className="bg-body-tertiary">
+    <Navbar
+      bg="dark"
+      data-bs-theme="dark"
+      expand="lg "
+      className="bg-body-tertiary"
+    >
       <Container>
-        <Navbar.Brand ><a href="https://www.apple.com/" target="blank"><img src={logo} alt="apple.logo" /></a></Navbar.Brand>
+        <Navbar.Brand>
+          <a href="https://www.apple.com/" target="blank">
+            <img src={logo} alt="apple.logo" />
+          </a>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto w-25">
-          <Nav.Link ><button onClick={() => handleCategoryFilter("")}>All</button> </Nav.Link>
-            <Nav.Link ><button onClick={() => handleCategoryFilter("smartphone")}>Iphone</button> </Nav.Link>
-            <Nav.Link ><button onClick={() => handleCategoryFilter("laptop")}>Mac</button> </Nav.Link>
-            <Nav.Link ><button onClick={() => handleCategoryFilter("tablet")}>Ipad</button> </Nav.Link>
-            <Nav.Link><button onClick={() => handleCategoryFilter("others")}>Others</button></Nav.Link>
-            
-            
+            <Nav.Link>
+              <button
+                onClick={() => handleCategoryFilter("")}
+                className={activeCategory === "" ? "active" : ""}
+              >
+                All
+              </button>{" "}
+            </Nav.Link>
+            <Nav.Link>
+              <button onClick={() => handleCategoryFilter("smartphone")} className={activeCategory === "smartphone" ? "active" : ""}
+              >
+                Iphone
+              </button>{" "}
+            </Nav.Link>
+            <Nav.Link>
+              <button
+                onClick={() => handleCategoryFilter("laptop")}
+                className={activeCategory === "laptop" ? "active" : ""}
+              >
+                Mac
+              </button>{" "}
+            </Nav.Link>
+            <Nav.Link>
+              <button
+                onClick={() => handleCategoryFilter("tablet")}
+                className={activeCategory === "tablet" ? "active" : ""}
+              >
+                Ipad
+              </button>{" "}
+            </Nav.Link>
+            <Nav.Link>
+              <button
+                onClick={() => handleCategoryFilter("others")}
+                className={activeCategory === "others" ? "active" : ""}
+              >
+                Others
+              </button>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
-
   );
 };
 export default MyNavbar;
 
-{/* <div className="navbar">
+{
+  /* <div className="navbar">
       
       <div className="logo">
         <img src={logo} alt="apple.logo" />
@@ -54,4 +93,5 @@ export default MyNavbar;
         <img src={logo} alt="horoscope" />
       </div>
 
-    </div> */}
+    </div> */
+}
